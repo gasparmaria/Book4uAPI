60aa8e8c4298a61e1428b37a151a10187fc4c2c2
Maria Eduarda Gaspar <73003909+gasparmaria@users.noreply.github.com>
2022-09-20 23:14:08 -0300

    Update script

--- script.sql
+++ script.sql
@@ -2,23 +2,22 @@ CREATE DATABASE dbBook4u;
 USE dbBook4u;
 
 CREATE TABLE tbLivro(
-	idLivro INT PRIMARY KEY AUTO_INCREMENT,
-    detalhes VARCHAR(150) NOT NULL,
-    numPag INT NOT NULL,
+    idLivro VARCHAR NOT NULL,
     titulo VARCHAR(100) NOT NULL,
-    autor VARCHAR(100) NOT NULL
+    autor VARCHAR(100) NOT NULL,
+    link VARCHAR
 );
 
 CREATE TABLE tbUsuario(
-	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     login VARCHAR(50) NOT NULL,
     senha VARCHAR(20) NOT NULL,
     nome VARCHAR (100) NOT NULL
 );
 
 CREATE TABLE tbLivroUsuario(
-	idLivro INT NOT NULL,
-    idUsuario INT NOT NULL,
-    CONSTRAINT fkLivro FOREIGN KEY (idLivro) REFERENCES tbLivro (idLivro),
-    CONSTRAINT fkUsuario FOREIGN KEY (idUsuario) REFERENCES tbUsuario (idUsuario)
+    fkUsuario INT NOT NULL,
+    fkLivro VARCHAR NOT NULL,
+    FOREIGN KEY (fkLivro) REFERENCES tbLivro (idLivro),
+    FOREIGN KEY (fkUsuario) REFERENCES tbUsuario (idUsuario)
 );
